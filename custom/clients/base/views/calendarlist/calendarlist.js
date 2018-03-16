e29406e0b4015b7bc14fa0a0375e010632960b83
tuckdesign <contact@tuckdesign.com>
2018-03-16 16:39:04 +0100

    RR-30: Split calendarlist methods to be defined per module

--- custom/clients/base/views/calendarlist/calendarlist.js
+++ custom/clients/base/views/calendarlist/calendarlist.js
@@ -1,4 +1,8 @@
 ({
+    /*
+     * @property calenarOptions - extra options for fullCalendar init (if needed)
+     */
+    calendarOptions: {},
     
     calendar: null,
     start: null,
@@ -36,10 +40,10 @@
             headDomNode.appendChild(cssNode);
         }
         this.collection.on('data:sync:complete', _.bind(this.updateCalendarData, this));
-        this.calendar = $('#calendar').fullCalendar({
+        this.calendar = $('#calendar').fullCalendar(_.extend({
             events: _.bind(this.getEvents, this),
             eventClick: _.bind(this.eventClick, this),
-        });
+        }, this.calendarOptions));
     },
     
     getEvents: function(start, end, timezone, callback) {
@@ -83,80 +87,31 @@
         this.calendar.fullCalendar('refetchEvents');
     },
     
+    /*
+     * getEventsFromCollection - must override
+     *
+     * @returns Array of events for fullcalendar
+     */
     getEventsFromCollection: function() {
-        var start = this.start.clone();
-        var events = [];
-        while (true) {
-            var event = this.getDateEventFromCollection(start, this.collection);
-            if (event) {
-                events.push(event);
-            } else {
-                events.push({
-                                title: 'Inactive',
-                                allDay: true,
-                                start: start.formatServer(),
-                                backgroundColor: '#D6D6D6',
-                                textColor: '#ffffff',
-                            });
-            }
-            if (start.isSame(this.end)) break;
-            start.add(1, 'days');
-        }
-        return events;
+        return [];
     },
     
-    getDateEventFromCollection: function(date, collection) {
-        var event = null;
-        var dateS = date.formatServer();
-        for (var i in collection.models) {
-            if (collection.models[i].get('date_field_c')==dateS) {
-                var title = collection.models[i].get('active_c');
-                title += "\nStart: " + collection.models[i].get('start_time_c');
-                title += "\nEnd: " + collection.models[i].get('end_time_c');
-                event = {
-                                id: collection.models[i].id,
-                                title: title,
-                                allDay: true,
-                                start: collection.models[i].get('date_field_c'),
-                                backgroundColor: (collection.models[i].get('active_c')=='Active')?'#ffffff':'#D6D6D6',
-                                textColor: '#000000',
-                            };
-                break;
-            }
-        }
-        return event;
-    },
-    
+    /*
+     * eventClick - called on calendar event click - should override
+     *
+     * @param Object calEvent - calendar event object
+     *
+     */
     eventClick: function(calEvent, jsEvent, view) {
-        if (calEvent.id) {
-            app.router.navigate("#" + this.module + "/" + calEvent.id, {trigger: true});
-        } else {
-            if (this._gettingDayRecord[calEvent._id]) return;
-            this._gettingDayRecord[calEvent._id] = true;
-            app.api.call('read', app.api.buildURL('RRAPT_Admin', '?filter[0][date_field_c]='+calEvent.start.formatServer()), {}, {
-                success: _.bind(function(data) {
-                    delete this._gettingDayRecord[calEvent._id];
-                    if (data && data.records && data.records.length) {
-                        app.router.navigate("#" + this.module + "/" + data.records[0].id, {trigger: true});
-                    } else {
-                        app.drawer.open({
-                                layout: 'create',
-                                context: {
-                                    create: true,
-                                    module: this.module,
-                                    model: this.setBeanDataOnCreate(calEvent),
-                                }
-                            },
-                            _.bind(function() {
-                                this.collection.fetch({});
-                            }, this)
-                        );
-                    }
-                }, this)
-            });
-        }
     },
     
+    /*
+     * setBeanDataOnCreate - helper function for create drawer to pass on some fields to create view (eg date)
+     *
+     * @param Object calEvents - calendar event object
+     *
+     * @return Bean bean - sugar bean for this.module
+     */
     setBeanDataOnCreate: function(calEvent) {
         var bean = app.data.createBean(this.module);
         return bean;
--- custom/modules/RRAPT_Admin/clients/base/views/calendarlist/calendarlist.js
+++ custom/modules/RRAPT_Admin/clients/base/views/calendarlist/calendarlist.js
@@ -10,4 +10,85 @@
         bean.set('date_field_c', calEvent.start.formatServer());
         return bean;
     },
+
+    /*
+     * @override
+     */
+    eventClick: function(calEvent) {
+        if (calEvent.id) {
+            app.router.navigate("#" + this.module + "/" + calEvent.id, {trigger: true});
+        } else {
+            if (this._gettingDayRecord[calEvent._id]) return;
+            this._gettingDayRecord[calEvent._id] = true;
+            app.api.call('read', app.api.buildURL('RRAPT_Admin', '?filter[0][date_field_c]='+calEvent.start.formatServer()), {}, {
+                success: _.bind(function(data) {
+                    delete this._gettingDayRecord[calEvent._id];
+                    if (data && data.records && data.records.length) {
+                        app.router.navigate("#" + this.module + "/" + data.records[0].id, {trigger: true});
+                    } else {
+                        app.drawer.open({
+                                layout: 'create',
+                                context: {
+                                    create: true,
+                                    module: this.module,
+                                    model: this.setBeanDataOnCreate(calEvent),
+                                }
+                            },
+                            _.bind(function() {
+                                this.collection.fetch({});
+                            }, this)
+                        );
+                    }
+                }, this)
+            });
+        }
+    },
+    
+    /*
+     * @override
+     */
+    getEventsFromCollection: function() {
+        var start = this.start.clone();
+        var events = [];
+        while (true) {
+            var event = this.getDateEventFromCollection(start, this.collection);
+            if (event) {
+                events.push(event);
+            } else {
+                events.push({
+                                title: 'Inactive',
+                                allDay: true,
+                                start: start.formatServer(),
+                                backgroundColor: '#D6D6D6',
+                                textColor: '#ffffff',
+                            });
+            }
+            if (start.isSame(this.end)) break;
+            start.add(1, 'days');
+        }
+        return events;
+    },
+    
+    getDateEventFromCollection: function(date, collection) {
+        var event = null;
+        var dateS = date.formatServer();
+        for (var i in collection.models) {
+            if (collection.models[i].get('date_field_c')==dateS) {
+                var title = collection.models[i].get('active_c');
+                title += "\nStart: " + collection.models[i].get('start_time_c');
+                title += "\nEnd: " + collection.models[i].get('end_time_c');
+                event = {
+                                id: collection.models[i].id,
+                                title: title,
+                                allDay: true,
+                                start: collection.models[i].get('date_field_c'),
+                                backgroundColor: (collection.models[i].get('active_c')=='Active')?'#ffffff':'#D6D6D6',
+                                textColor: '#000000',
+                            };
+                break;
+            }
+        }
+        return event;
+    },
+    
 })
