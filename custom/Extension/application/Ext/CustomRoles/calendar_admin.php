96b9158cce1b6e2638e486e83dfb008bf5c60fc3
tuckdesign <contact@tuckdesign.com>
2018-03-28 11:49:49 +0200

    RR-36: basic roles

--- custom/Extension/application/Ext/CustomRoles/calendar_admin.php
+++ custom/Extension/application/Ext/CustomRoles/calendar_admin.php
@@ -3,5 +3,18 @@
 $customRoles['calendar_admin'] = array(
     'name' => 'Calendar Admin', 
     'description' => 'Calendar Admin', 
+    'modules' => array(
+        'RRAPT_Calendar' => array(
+            'access' => 'enabled', // enabled/disabled
+            'admin' => 'normal', // normal/admin/developer/admin_developer
+            'delete' => 'owner', // all/owner/none
+            'edit' => 'owner', // all/owner/none
+            'export' => 'owner', // all/owner/none
+            'import' => 'owner', // all/owner/none
+            'list' => 'all', // all/owner/none
+            'massupdate' => 'none', // all/none
+            'view' => 'all', // all/owner/none
+        ),
+    ),
 );
  
--- custom/Extension/application/Ext/CustomRoles/closer.php
+++ custom/Extension/application/Ext/CustomRoles/closer.php
@@ -2,6 +2,19 @@
 
 $customRoles['closer'] = array(
     'name' => 'Closer', 
-    'description' => 'Closer users', 
+    'description' => 'Closer users',
+    'modules' => array(
+        'RRAPT_Calendar' => array(
+            'access' => 'enabled', // enabled/disabled
+            'admin' => 'normal', // normal/admin/developer/admin_developer
+            'delete' => 'owner', // all/owner/none
+            'edit' => 'owner', // all/owner/none
+            'export' => 'owner', // all/owner/none
+            'import' => 'owner', // all/owner/none
+            'list' => 'all', // all/owner/none
+            'massupdate' => 'none', // all/none
+            'view' => 'all', // all/owner/none
+        ),
+    ),
 );
  
