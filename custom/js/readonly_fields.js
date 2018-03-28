2e5032717d85f8dd951cc88e940f1e526e565275
tuckdesign <contact@tuckdesign.com>
2018-03-28 08:58:14 +0200

    RR-36: Readonly plugin for recordlist also

--- custom/js/readonly_fields.js
+++ custom/js/readonly_fields.js
@@ -12,18 +12,30 @@
              * @inheritdoc
              */
             onAttach: function() {
-                this.on('init', _.bind(function() {
-                    if (this.readOnlyFields && this.readOnlyFields.length) {
-                        for (var i in this.readOnlyFields) {
-                            this._readOnlyFields.push(this.readOnlyFields[i]);
-                        }
-                        for (var i in this.meta.panels) {
-                            for (var j in this.meta.panels[i].fields) {
-                                if (this.meta.panels[i] && this.meta.panels[i].fields[j] && this.meta.panels[i].fields[j].name && _.contains(this._readOnlyFields, this.meta.panels[i].fields[j].name)) this.meta.panels[i].fields[j].readonly = true;
-                            }
-                        }
+                if (this.readOnlyFields && this.readOnlyFields.length) {
+                    for (var i in this.readOnlyFields) {
+                        this._readOnlyFields.push(this.readOnlyFields[i]);
                     }
-                }, this));
+                    if (typeof(this.parseFields)=='function') {
+                        var oldParseFields = this.parseFields;
+                        this.parseFields = _.bind(function() {
+                                                    for (var i in this.meta.panels) {
+                                                        for (var j in this.meta.panels[i].fields) {
+                                                            if (this.meta.panels[i] && this.meta.panels[i].fields[j] && this.meta.panels[i].fields[j].name && _.contains(this._readOnlyFields, this.meta.panels[i].fields[j].name)) this.meta.panels[i].fields[j].readonly = true;
+                                                        }
+                                                    }
+                                                    oldParseFields.apply(this, arguments);
+                                                }, this);
+                    } else {
+                        this.on('init', _.bind(function() {
+                            for (var i in this.meta.panels) {
+                                for (var j in this.meta.panels[i].fields) {
+                                    if (this.meta.panels[i] && this.meta.panels[i].fields[j] && this.meta.panels[i].fields[j].name && _.contains(this._readOnlyFields, this.meta.panels[i].fields[j].name)) this.meta.panels[i].fields[j].readonly = true;
+                                }
+                            }
+                        }, this);
+                    }
+                }
             },
 
         });
