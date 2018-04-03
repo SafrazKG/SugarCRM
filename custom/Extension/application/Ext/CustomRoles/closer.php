5ddb0cc4ac8e58ff3ec283c5059b818c62af0b86
tuckdesign <contact@tuckdesign.com>
2018-04-03 16:06:23 +0200

    Implemented roles

--- custom/Extension/application/Ext/CustomRoles/closer.php
+++ custom/Extension/application/Ext/CustomRoles/closer.php
@@ -7,14 +7,30 @@ $customRoles['closer'] = array(
         'RRAPT_Calendar' => array(
             'access' => 'enabled', // enabled/disabled
             'admin' => 'normal', // normal/admin/developer/admin_developer
-            'delete' => 'owner', // all/owner/none
+            'delete' => 'none', // all/owner/none
             'edit' => 'owner', // all/owner/none
-            'export' => 'owner', // all/owner/none
-            'import' => 'owner', // all/owner/none
-            'list' => 'all', // all/owner/none
+            'export' => 'none', // all/owner/none
+            'import' => 'none', // all/owner/none
+            'list' => 'owner', // all/owner/none
             'massupdate' => 'none', // all/none
-            'view' => 'all', // all/owner/none
+            'view' => 'owner', // all/owner/none
+            'fields' => array(
+                'assigned_user_name' => 'readonly',
+                'users_rrapt_calendar_1_name' => 'readonly',
+                'users_rrapt_calendar_3_name' => 'readonly',
+            ),
         ),
+        'RRAPT_Admin' => array(
+            'access' => 'disabled',
+            'admin' => 'normal', // normal/admin/developer/admin_developer
+            'delete' => 'none', // all/owner/none
+            'edit' => 'none', // all/owner/none
+            'export' => 'none', // all/owner/none
+            'import' => 'none', // all/owner/none
+            'list' => 'none', // all/owner/none
+            'massupdate' => 'none', // all/none
+            'view' => 'none', // all/owner/none
+        )
     ),
 );
  
--- modules/RRAPT_Admin/RRAPT_Admin.php
+++ modules/RRAPT_Admin/RRAPT_Admin.php
@@ -14,4 +14,52 @@
  */
 require_once('modules/RRAPT_Admin/RRAPT_Admin_sugar.php');
 class RRAPT_Admin extends RRAPT_Admin_sugar {
+    
+    public function ACLAccess($view, $context = null) {
+        if (in_array($view, ['list']) && $this->isFromCalendarApi() && ($this->hasRole('fronter') || $this->hasRole('closer') || $this->hasRole('calendar_admin'))) {
+            return true;
+        }
+        return parent::ACLAccess($view, $context);
+    }
+
+    /*
+     * Helper function to get user's role ids
+     *
+     * @return array
+     * 
+     */    
+    public function getUserRoleIds() {
+        if (!isset($GLOBALS['GetUserRolesIds'])) {
+            $ids = [];
+            $roles = ACLRole::getUserRoles($GLOBALS['current_user']->id, false);
+            foreach ($roles as $r) {
+                $ids[] = $r->id;
+            }
+            $GLOBALS['GetUserRolesIds'] = $ids;
+        }
+        return $GLOBALS['GetUserRolesIds'];
+    }
+    
+    /*
+     * Helper function to check does user belongs to specific role
+     *
+     * @return boolean
+     * 
+     */    
+    public function hasRole($role, $roles = null) {
+        if (!$roles) $roles = $this->getUserRoleIds();
+        foreach ($roles as $r) {
+            if ($r==$role) return true;
+        }
+        return false;
+    }
+    
+    private function isFromCalendarApi() {
+        $debug = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS);
+        foreach ($debug as $arr) {
+            if ($arr['class']=='CalendarFilterApi') return true;
+        }
+        return false;
+    }
+
 }
