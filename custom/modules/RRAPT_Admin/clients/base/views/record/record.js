b6e8c8d52de353ce37fc571491375c63150d50fc
tuckdesign <contact@tuckdesign.com>
2018-04-07 17:59:06 +0200

    RR-30: Implemented custom messages

--- custom/modules/RRAPT_Admin/clients/base/views/record/record.js
+++ custom/modules/RRAPT_Admin/clients/base/views/record/record.js
@@ -1,9 +1,27 @@
 ({
     extendsFrom: 'RecordView',
     
+    _originalActive: '',
+    
     initialize: function() {
         this.plugins = _.union(this.plugins||[], ['CalendarTimes']);
         this._super('initialize', arguments);
+        this.model.on('data:sync:complete', _.bind(function() {
+            this._originalActive = this.model.get('active_c');
+        }, this));
     },
-    
+
+    getCustomSaveOptions: function(options) {
+        options = this._super('getCustomSaveOptions', [options]);
+        var msg = 'You have successfully saved this date.';
+        if (this.model.get('active_c')=='Active' && this.model.get('active_c')!=this._originalActive) msg = 'You have successfully marked this date as Active';
+        options.showAlerts = {
+                'process': true,
+                'success': {
+                    messages: msg
+                }
+            };
+        return options;
+    }
+
 })
