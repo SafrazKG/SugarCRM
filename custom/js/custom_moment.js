7b7861d55685c3e4ab4153937032fb37c7f23ca0
tuckdesign <contact@tuckdesign.com>
2018-04-11 15:12:01 +0200

    bugfix

--- custom/js/custom_moment.js
+++ custom/js/custom_moment.js
@@ -5,6 +5,7 @@
         date.prototype.removeTZ = function(timeString) {
             var t = timeString.split('T');
             var date = t[0];
+            if (typeof(t[1])=='undefined') return date;
             var time = t[1].split('+')[0].split('-')[0];
             return date + 'T' + time;
         };
