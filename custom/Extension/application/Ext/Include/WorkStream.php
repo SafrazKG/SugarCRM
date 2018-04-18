8d5d60b4c2a7a6aca3eda689099346c0ed1bd5c1
Sérgio Peixoto <sergio@intelestream.net>
2018-04-18 18:00:27 +0100

    RR-40: Adding missing include files to activate workstream modules

---
+++ custom/Extension/application/Ext/Include/WorkStream.php
@@ -0,0 +1,7 @@
+<?php 
+ //WARNING: The contents of this file are auto-generated
+$beanList['INTWS_WorkStreams'] = 'INTWS_WorkStreams';
+$beanFiles['INTWS_WorkStreams'] = 'modules/INTWS_WorkStreams/INTWS_WorkStreams.php';
+$moduleList[] = 'INTWS_WorkStreams';
+
+?>
