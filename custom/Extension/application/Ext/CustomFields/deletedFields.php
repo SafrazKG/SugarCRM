98f49f782c29dfdf560eb998af3dbc77ddef62ac
tuckdesign <contact@tuckdesign.com>
2018-04-30 15:09:34 +0200

    Removed deleted fields

--- custom/Extension/application/Ext/CustomFields/deletedFields.php
+++ custom/Extension/application/Ext/CustomFields/deletedFields.php
@@ -1,3 +1,13 @@
 <?php
 
 $deleteFields[] = 'Leadslead_status_c';
+$deleteFields[] = 'abc_Estoppelsmaintenance_fees_information_c';
+$deleteFields[] = 'abc_Estoppelsproperty_information_c';
+$deleteFields[] = 'abc_Maintenance_Feestype_b_c';
+$deleteFields[] = 'abc_Mortgagesweek_will_use_a_c';
+$deleteFields[] = 'abc_Mortgagesweek_will_use_b_c';
+$deleteFields[] = 'abc_Mortgagesweek_will_use_c';
+$deleteFields[] = 'abc_Transfersweek_will_use_a_c';
+$deleteFields[] = 'abc_Transfersweek_will_use_b_c';
+$deleteFields[] = 'abc_Transfersweek_will_use_c';
+$deleteFields[] = 'RRDIS_Dispositionsstatus_c';
